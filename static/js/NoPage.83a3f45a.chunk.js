6c11c83139c311d3ccb7ce535bb39f5f3a4d1ee8
Vitaliy28031985 <94307584+Vitaliy28031985@users.noreply.github.com>
2022-07-29 12:20:48 +0000

    Deploying to gh-pages from @ Vitaliy28031985/goit-react-hw-05-movies@77eddb7070ca45c7019a3fe7038ef41a94280d10 🚀

---
+++ static/js/NoPage.83a3f45a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[632],{698:function(e,t,n){n.r(t),n.d(t,{NoPage:function(){return o}});var i=n(184),o=function(){return(0,i.jsx)("div",{children:(0,i.jsx)("p",{style:{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontSize:40,color:"blak"},children:"Page not found!!!"})})}}}]);
+//# sourceMappingURL=NoPage.83a3f45a.chunk.js.map
