94ba5bac9b11f96c6218592e35b235ef4bcd5d27
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-07-31 14:08:27 +0300

    loder 31

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,9 +12,9 @@ const Cast = lazy(() => import('components/Cast/Cast' /* webpackChunkName: "Cast
 const Reviews = lazy(() => import('components/Reviews/Reviews' /* webpackChunkName: "Reviews" */));
 const NoPage = lazy(() => import('components/NoPage/NoPage' /* webpackChunkName: "NoPage" */));
 
-// const LoaderComponent = lazy(() => import('components/Loader/Loader' /* webpackChunkName: "Loader" */));
+const LoaderComponent = lazy(() => import('components/Loader/Loader' /* webpackChunkName: "Loader" */));
 
-//<LoaderComponent/>
+// <p>Loder...</p> <LoaderComponent/>
 
 export const App = () => {
 
@@ -22,7 +22,7 @@ export const App = () => {
     <>
  
    <Navigation/>
-<Suspense fallback={<p>Loder...</p>}>
+<Suspense fallback={<LoaderComponent/>}>
 <Routes>
 
 <Route  path="/" element={<Home/>}/>
